ed266624e0476a79fc0136f89199f0f47a72c368
paycheckpacheck <140981896+paycheckpacheck@users.noreply.github.com>
2025-03-11 17:33:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    RX_DATA = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    basic.showIcon(IconNames.Yes)
+    control.waitMicros(400000)
+    serial.writeLine("" + RX_DATA + "\\n")
+})
+let RX_DATA = ""
+serial.setBaudRate(BaudRate.BaudRate19200)
+basic.forever(function () {
+    basic.showIcon(IconNames.No)
+})
