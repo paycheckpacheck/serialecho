c5bf7e95058c7bc115fa0216007f84b5c9944019
paycheckpacheck <140981896+paycheckpacheck@users.noreply.github.com>
2025-03-11 17:47:39 -0500

    updated ig

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     RX_DATA = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     basic.showIcon(IconNames.Yes)
-    control.waitMicros(400000)
-    serial.writeLine("" + RX_DATA + "\\n")
+    control.waitMicros(10000)
+    serial.writeLine(RX_DATA)
 })
 let RX_DATA = ""
 serial.setBaudRate(BaudRate.BaudRate19200)
