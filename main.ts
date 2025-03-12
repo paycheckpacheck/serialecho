af7928ce5a0b7426aa41a9a641630720a13390be
paycheckpacheck <140981896+paycheckpacheck@users.noreply.github.com>
2025-03-11 20:17:55 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
+enum RadioMessage {
+    message1 = 49434
+}
+radio.onReceivedString(function (receivedString) {
+    control.waitMicros(10000)
+    basic.showIcon(IconNames.Yes)
+    serial.writeLine(receivedString)
+})
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     RX_DATA = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     basic.showIcon(IconNames.Yes)
     control.waitMicros(10000)
-    serial.writeLine(RX_DATA)
+    radio.sendString(RX_DATA)
 })
 let RX_DATA = ""
+radio.setGroup(1)
+radio.setTransmitPower(7)
 serial.setBaudRate(BaudRate.BaudRate19200)
 basic.forever(function () {
     basic.showIcon(IconNames.No)
